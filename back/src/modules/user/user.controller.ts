0604e7608bd4d8ffff0cdba54cf8e90fc14bca6d
Mariem BenYounes <maryemby21@gmail.com>
2024-11-13 20:37:24 +0100

    added post endpoint

--- back/src/modules/user/user.controller.ts
+++ back/src/modules/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,Body } from '@nestjs/common';
+import { AddUserDTO } from './dto/add-user.dto';
+import {User} from '../user/user.entity'
+import {UserService} from '../user/user.service'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post("/register")
+    registerUser(@Body() addUserDTO: AddUserDTO):User {
+        const user:User=this.userService.addUser(addUserDTO);
+        return user;
+    }
+}
--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { AddUserDTO } from './dto/add-user.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+    @InjectRepository(User)
+    private repository: Repository<User>
+ ){}
+
+    addUser(addUserDTO: AddUserDTO):User {
+
+        const dto_2_user: User = {
+            id: 1,
+            username:addUserDTO.username,
+            email: addUserDTO.email,
+            password: addUserDTO.password,
+            phone:addUserDTO.phone,
+            verifiedAt:null,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        const newUser=this.repository.create(dto_2_user);
+        this.repository.save(newUser);
+        return newUser;
+    }
+}

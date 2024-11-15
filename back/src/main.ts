3f3422684af0fe0f0ac63a8f48c82cfccfc0c0f7
Mohamed amine Tlili <foxweb585@gmail.com>
2024-11-15 16:21:20 +0100

    adapt develop

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,10 +6,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe(
-     whitelist: true,
-    forbidNonWhitelisted: true
-  ));
+  app.useGlobalPipes();
 
   await app.listen(process.env.PORT ?? 3000);
 }

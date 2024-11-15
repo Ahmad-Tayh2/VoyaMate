709cd1adcecf9d4de65bf44dca80361f2b03b87c
foued trabelsi <106319972+gitfoued@users.noreply.github.com>
2024-11-15 09:13:41 +0100

    Update main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe(
+     whitelist: true,
+    forbidNonWhitelisted: true
+  ));
 
   await app.listen(process.env.PORT ?? 3000);
 }

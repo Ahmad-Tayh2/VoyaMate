85396af899b7c47cb9acb156ea029c15e19820a6
Ahmad-Tayh2 <ahmad.tayh2@gmail.com>
2024-11-24 20:41:44 +0100

    chore: import checkpoint module

--- back/src/modules/checkpoint/checkpoint.module.ts
+++ back/src/modules/checkpoint/checkpoint.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Checkpoint } from './checkpoint.entity';
 import { CheckpointService } from './checkpoint.service';
 import { CheckpointController } from './checkpoint.controller';
+import { ItineraryModule } from '../itinerary/itinerary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Checkpoint])],
+  imports: [TypeOrmModule.forFeature([Checkpoint]), ItineraryModule],
   controllers: [CheckpointController],
   providers: [CheckpointService],
 })
